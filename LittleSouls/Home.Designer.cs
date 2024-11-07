536dbcc0b2bc4bb4ee38ab56e9257aae6e02b788
bayan almerri <156858990+Bayan-03@users.noreply.github.com>
2024-11-07 17:33:57 +0300

    guna pkg

--- LittleSouls/Home.Designer.cs
+++ LittleSouls/Home.Designer.cs
@@ -28,12 +28,272 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.petsBtn1 = new LittleSouls.PetsBtn();
+            this.petsBtn2 = new LittleSouls.PetsBtn();
+            this.petsBtn3 = new LittleSouls.PetsBtn();
+            this.petsBtn6 = new LittleSouls.PetsBtn();
+            this.petsBtn5 = new LittleSouls.PetsBtn();
+            this.petsBtn7 = new LittleSouls.PetsBtn();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel8.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(207)))), ((int)(((byte)(218)))));
+            this.flowLayoutPanel1.Controls.Add(this.panel6);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel4);
+            this.flowLayoutPanel1.Controls.Add(this.panel7);
+            this.flowLayoutPanel1.Controls.Add(this.panel5);
+            this.flowLayoutPanel1.Controls.Add(this.panel8);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 30, 3, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(278, 450);
+            this.flowLayoutPanel1.TabIndex = 8;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(3, 3);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(275, 132);
+            this.panel6.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.petsBtn1);
+            this.panel2.Location = new System.Drawing.Point(3, 145);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(275, 40);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.petsBtn2);
+            this.panel1.Location = new System.Drawing.Point(3, 195);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(275, 40);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.petsBtn3);
+            this.panel4.Location = new System.Drawing.Point(3, 245);
+            this.panel4.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(275, 40);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.petsBtn6);
+            this.panel7.Location = new System.Drawing.Point(3, 295);
+            this.panel7.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(275, 40);
+            this.panel7.TabIndex = 8;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.petsBtn5);
+            this.panel5.Location = new System.Drawing.Point(3, 408);
+            this.panel5.Margin = new System.Windows.Forms.Padding(3, 70, 3, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(275, 40);
+            this.panel5.TabIndex = 7;
+            // 
+            // panel8
+            // 
+            this.panel8.Controls.Add(this.pictureBox2);
+            this.panel8.Controls.Add(this.petsBtn7);
+            this.panel8.Location = new System.Drawing.Point(3, 458);
+            this.panel8.Margin = new System.Windows.Forms.Padding(3, 7, 3, 3);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(275, 53);
+            this.panel8.TabIndex = 9;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 53);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Graphik Arabic Medium", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(92)))), ((int)(((byte)(199)))));
+            this.label1.Location = new System.Drawing.Point(478, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 44);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Home";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // petsBtn1
+            // 
+            this.petsBtn1.FlatAppearance.BorderSize = 0;
+            this.petsBtn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.petsBtn1.Font = new System.Drawing.Font("Graphik Arabic SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsBtn1.HoverForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn1.HoverImage1 = null;
+            this.petsBtn1.Location = new System.Drawing.Point(0, 3);
+            this.petsBtn1.Name = "petsBtn1";
+            this.petsBtn1.NormalForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn1.NormalImage1 = null;
+            this.petsBtn1.Size = new System.Drawing.Size(272, 37);
+            this.petsBtn1.TabIndex = 0;
+            this.petsBtn1.Text = "Pets";
+            this.petsBtn1.UseVisualStyleBackColor = true;
+            // 
+            // petsBtn2
+            // 
+            this.petsBtn2.FlatAppearance.BorderSize = 0;
+            this.petsBtn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.petsBtn2.Font = new System.Drawing.Font("Graphik Arabic SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsBtn2.HoverForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn2.HoverImage1 = null;
+            this.petsBtn2.Location = new System.Drawing.Point(0, 3);
+            this.petsBtn2.Name = "petsBtn2";
+            this.petsBtn2.NormalForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn2.NormalImage1 = null;
+            this.petsBtn2.Size = new System.Drawing.Size(272, 37);
+            this.petsBtn2.TabIndex = 0;
+            this.petsBtn2.Text = "Adopt";
+            this.petsBtn2.UseVisualStyleBackColor = true;
+            // 
+            // petsBtn3
+            // 
+            this.petsBtn3.FlatAppearance.BorderSize = 0;
+            this.petsBtn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.petsBtn3.Font = new System.Drawing.Font("Graphik Arabic SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsBtn3.HoverForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn3.HoverImage1 = null;
+            this.petsBtn3.Location = new System.Drawing.Point(0, 3);
+            this.petsBtn3.Name = "petsBtn3";
+            this.petsBtn3.NormalForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn3.NormalImage1 = null;
+            this.petsBtn3.Size = new System.Drawing.Size(272, 37);
+            this.petsBtn3.TabIndex = 0;
+            this.petsBtn3.Text = "Invoises";
+            this.petsBtn3.UseVisualStyleBackColor = true;
+            // 
+            // petsBtn6
+            // 
+            this.petsBtn6.FlatAppearance.BorderSize = 0;
+            this.petsBtn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.petsBtn6.Font = new System.Drawing.Font("Graphik Arabic SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsBtn6.HoverForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn6.HoverImage1 = null;
+            this.petsBtn6.Location = new System.Drawing.Point(0, 3);
+            this.petsBtn6.Name = "petsBtn6";
+            this.petsBtn6.NormalForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn6.NormalImage1 = null;
+            this.petsBtn6.Size = new System.Drawing.Size(272, 37);
+            this.petsBtn6.TabIndex = 0;
+            this.petsBtn6.Text = "Storage";
+            this.petsBtn6.UseVisualStyleBackColor = true;
+            // 
+            // petsBtn5
+            // 
+            this.petsBtn5.FlatAppearance.BorderSize = 0;
+            this.petsBtn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.petsBtn5.Font = new System.Drawing.Font("Graphik Arabic SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsBtn5.HoverForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn5.HoverImage1 = null;
+            this.petsBtn5.Location = new System.Drawing.Point(0, 3);
+            this.petsBtn5.Name = "petsBtn5";
+            this.petsBtn5.NormalForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn5.NormalImage1 = null;
+            this.petsBtn5.Size = new System.Drawing.Size(272, 37);
+            this.petsBtn5.TabIndex = 0;
+            this.petsBtn5.Text = "Setting";
+            this.petsBtn5.UseVisualStyleBackColor = true;
+            // 
+            // petsBtn7
+            // 
+            this.petsBtn7.FlatAppearance.BorderSize = 0;
+            this.petsBtn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.petsBtn7.Font = new System.Drawing.Font("Graphik Arabic SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.petsBtn7.HoverForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn7.HoverImage1 = null;
+            this.petsBtn7.Location = new System.Drawing.Point(0, 3);
+            this.petsBtn7.Name = "petsBtn7";
+            this.petsBtn7.NormalForecolor1 = System.Drawing.Color.Empty;
+            this.petsBtn7.NormalImage1 = null;
+            this.petsBtn7.Size = new System.Drawing.Size(272, 47);
+            this.petsBtn7.TabIndex = 0;
+            this.petsBtn7.Text = "Profile";
+            this.petsBtn7.UseVisualStyleBackColor = true;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::LittleSouls.Properties.Resources.الخلفية_;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Name = "Home";
             this.Text = "Home";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel8.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel2;
+        private PetsBtn petsBtn1;
+        private System.Windows.Forms.Panel panel1;
+        private PetsBtn petsBtn2;
+        private System.Windows.Forms.Panel panel4;
+        private PetsBtn petsBtn3;
+        private System.Windows.Forms.Panel panel7;
+        private PetsBtn petsBtn6;
+        private System.Windows.Forms.Panel panel5;
+        private PetsBtn petsBtn5;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private PetsBtn petsBtn7;
+        private System.Windows.Forms.Label label1;
     }
 }
