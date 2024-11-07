7d7c81a0826de17b9eb21704a06f6b9790e3a1f8
bayan almerri <156858990+Bayan-03@users.noreply.github.com>
2024-11-07 16:50:58 +0300

    adopt form
    empty

---
+++ LittleSouls/ِAdoption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LittleSouls
+{
+    public partial class _ِAdoption : Form
+    {
+        public _ِAdoption()
+        {
+            InitializeComponent();
+        }
+    }
+}
