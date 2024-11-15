3529be6b59d82b4fa4ac18bce686d773c535bd51
thfatima <fatimh18844@gmail.com>
2024-11-15 22:04:34 +0300

    button x

--- LittleSouls/Program.cs
+++ LittleSouls/Program.cs
@@ -16,7 +16,7 @@ namespace LittleSouls
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Adoption());
+            Application.Run(new login());
         }
     }
 }
