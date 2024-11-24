dcf167ee70de8e76ce9afa6f356ff2f5c15563d4
thfatima <fatimh18844@gmail.com>
2024-11-25 01:25:38 +0300

    storage code

--- LittleSouls/storgeForm.Designer.cs
+++ LittleSouls/storgeForm.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.btn_Save = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
             this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
@@ -87,34 +87,6 @@
             this.ComboBoxStorgeCategories.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.ComboBoxStorgeCategories.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
             this.ComboBoxStorgeCategories.ItemHeight = 30;
-            this.ComboBoxStorgeCategories.Items.AddRange(new object[] {
-            "Cat food",
-            "Rabbit food",
-            "Bird food",
-            "Fish food",
-            "Chicks food",
-            "Turtle food",
-            "Dogs food",
-            "Ducks food",
-            "Hamster food",
-            "Cat toys",
-            "Rabbit toys",
-            "Bird toys",
-            "Fish toys",
-            "Chicks toys",
-            "Turtle toys",
-            "Dogs toys",
-            "Ducks toys",
-            "Hamster toys",
-            "Cat supplies",
-            "Bird supplies",
-            "Fish supplies",
-            "Chick supplies",
-            "Turtle supplies",
-            "Duck supplies",
-            "Dog supplies",
-            "Hamster supplies",
-            "Rabbit supplies"});
             this.ComboBoxStorgeCategories.Location = new System.Drawing.Point(1105, 104);
             this.ComboBoxStorgeCategories.Name = "ComboBoxStorgeCategories";
             this.ComboBoxStorgeCategories.Size = new System.Drawing.Size(270, 36);
@@ -156,16 +128,17 @@
             this.dataGridViewStorage.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridViewStorage.BackgroundColor = System.Drawing.Color.White;
             this.dataGridViewStorage.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Graphik Arabic Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(207)))), ((int)(((byte)(218)))));
-            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridViewStorage.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Graphik Arabic Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(207)))), ((int)(((byte)(218)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewStorage.DefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridViewStorage.Location = new System.Drawing.Point(48, 175);
             this.dataGridViewStorage.Name = "dataGridViewStorage";
+            this.dataGridViewStorage.ReadOnly = true;
             this.dataGridViewStorage.RowHeadersWidth = 51;
             this.dataGridViewStorage.RowTemplate.Height = 35;
             this.dataGridViewStorage.Size = new System.Drawing.Size(1338, 443);
--- LittleSouls/storgeForm.cs
+++ LittleSouls/storgeForm.cs
@@ -14,36 +14,130 @@ namespace LittleSouls
     public partial class storageForm : Form
     {
 
+        littleSoulsEntities littleSouls = new littleSoulsEntities();
 
-
-        private string connectionString = Settings.ConnctionString; // عدل معلومات الاتصال
-        private SqlDataAdapter dataAdapter;
-        private DataTable dataTable;
+        //private string connectionString = Settings.ConnctionString; // عدل معلومات الاتصال
+        //private SqlDataAdapter dataAdapter;
+        //private DataTable dataTable;
         public storageForm()
         {
             InitializeComponent();
+            LoadAllData();
         }
+
+
+        private void LoadAllData()
+        {
+
+
+            ComboBoxStorgeCategories.Items.Add("Food");
+            ComboBoxStorgeCategories.Items.Add("Toys");
+            ComboBoxStorgeCategories.Items.Add("Supplies");
+
+            try
+            {
+                // جلب جميع البيانات من جدول الحيوانات باستخدام LINQ to Entities
+                var storageView = littleSouls.storage
+                    .Select(p => new
+                    {
+                        p.shipmentNo,
+                        p.itemCode,
+                        p.quantity,
+                        p.price,
+                        p.receivedDate,
+                        p.itemCondition,
+                        p.expirationDate,
+                        p.supplier
+                      
+                    })
+                    .ToList();
+
+                // ربط البيانات بـ DataGridView
+                dataGridViewStorage.DataSource =storageView;
+
+                // جلب أنواع الحيوانات الفريدة لعرضها في ComboBox
+                var StorgeCategorie = littleSouls.storage
+                    .Select(p => p.supplier)
+                    .Distinct()
+                    .ToList();
+
+                // إضافة خيار "الكل" كأول عنصر
+                StorgeCategorie.Insert(0, "All");
+
+                // ربط الأنواع بـ ComboBox
+                ComboBoxStorgeCategories.DataSource = StorgeCategorie;
+
+                // تعيين القيمة الافتراضية إلى "الكل"
+                ComboBoxStorgeCategories.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"حدث خطأ أثناء تحميل البيانات: {ex.Message}");
+            }
+        }
+
+
+        private void FilterPetsByType()
+        {
+            try
+            {
+                // جلب النوع المحدد من ComboBox
+                string selectedCategorie = ComboBoxStorgeCategories.SelectedItem?.ToString();
+
+                // استعلام قاعدة البيانات
+                var filteredStorageQuery = littleSouls.storage.AsQueryable();
+
+                // تطبيق الفلترة إذا لم يكن النوع "All"
+                if (!string.IsNullOrEmpty(selectedCategorie) && selectedCategorie!= "All")
+                {
+                    filteredStorageQuery= filteredStorageQuery.Where(p => p.supplier == selectedCategorie);
+                }
+
+                // جلب البيانات المطلوبة فقط
+                var filteredstorage = filteredStorageQuery
+                    .Select(p => new
+                    {
+                        p.shipmentNo,
+                        p.itemCode,
+                        p.quantity,
+                        p.price,
+                        p.receivedDate,
+                        p.itemCondition,
+                        p.expirationDate,
+                        p.supplier
+                    })
+                    .ToList();
+
+                // عرض البيانات في DataGridView
+                dataGridViewStorage.DataSource = filteredstorage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"حدث خطأ أثناء تصفية البيانات: {ex.Message}");
+            }
+        }
+
 
         private void storgeForm_Load(object sender, EventArgs e)
         {
 
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
-                    dataAdapter = new SqlDataAdapter("SELECT * FROM storage", connection);
-                    SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    try
+            //    {
+            //        connection.Open();
+            //        dataAdapter = new SqlDataAdapter("SELECT * FROM storage", connection);
+            //        SqlCommandBuilder commandBuilder = new SqlCommandBuilder(dataAdapter);
 
-                    dataTable = new DataTable();
-                    dataAdapter.Fill(dataTable);
-                    dataGridViewStorage.DataSource = dataTable;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error loading data: {ex.Message}");
-                }
-            }
+            //        dataTable = new DataTable();
+            //        dataAdapter.Fill(dataTable);
+            //        dataGridViewStorage.DataSource = dataTable;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show($"Error loading data: {ex.Message}");
+            //    }
+            //}
         }
 
         //private void SaveChanges()
@@ -66,91 +160,89 @@ namespace LittleSouls
 
         private void ComboBoxStorgeCategories_SelectedIndexChanged(object sender, EventArgs e)
         {
-            //string selectedCategory = ComboBoxStorgeCategories.SelectedItem.ToString();
-            //FilterData(selectedCategory);
-            // الحصول على قيمة itemName التي اختارها المستخدم من ComboBox
-            string selectedItemName = ComboBoxStorgeCategories.SelectedItem.ToString();
 
-            // تصفية البيانات بناءً على itemName
-            FilterDataByItemName(selectedItemName);
+            FilterPetsByType();
+
+
+         
         }
 
         private void FilterDataByItemName(string selectedItemName)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    try
+            //    {
+            //        connection.Open();
 
-                    // استعلام SQL مع JOIN بين جدول storage و items
-                    string query = @"
-                                   SELECT s.*
-                                   FROM storage s
-                                   INNER JOIN items i ON s.itemCode = i.itemCode
-                                   WHERE i.itemName = @selectedItemName";
+            //        // استعلام SQL مع JOIN بين جدول storage و items
+            //        string query = @"
+            //                       SELECT s.*
+            //                       FROM storage s
+            //                       INNER JOIN items i ON s.itemCode = i.itemCode
+            //                       WHERE i.itemName = @selectedItemName";
 
-                    SqlCommand command = new SqlCommand(query, connection);
-                    command.Parameters.AddWithValue("@selectedItemName", selectedItemName);  // القيمة المختارة من ComboBox
+            //        SqlCommand command = new SqlCommand(query, connection);
+            //        command.Parameters.AddWithValue("@selectedItemName", selectedItemName);  // القيمة المختارة من ComboBox
 
-                    SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
-                    DataTable dataTable = new DataTable();
-                    dataAdapter.Fill(dataTable);
+            //        SqlDataAdapter dataAdapter = new SqlDataAdapter(command);
+            //        DataTable dataTable = new DataTable();
+            //        dataAdapter.Fill(dataTable);
 
-                    // ربط DataGridView بالبيانات المصفاة
-                    dataGridViewStorage.DataSource = dataTable;
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error loading data: {ex.Message}");
-                }
-            }
+            //        // ربط DataGridView بالبيانات المصفاة
+            //        dataGridViewStorage.DataSource = dataTable;
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show($"Error loading data: {ex.Message}");
+            //    }
+            //}
         }
 
 
 
         private void LoadItemNamesIntoComboBox()
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                try
-                {
-                    connection.Open();
+            //using (SqlConnection connection = new SqlConnection(connectionString))
+            //{
+            //    try
+            //    {
+            //        connection.Open();
 
-                    // استعلام لتحميل itemNames من جدول items
-                    string query = "SELECT itemName FROM items";
-                    SqlCommand command = new SqlCommand(query, connection);
-                    SqlDataReader reader = command.ExecuteReader();
+            //        // استعلام لتحميل itemNames من جدول items
+            //        string query = "SELECT itemName FROM items";
+            //        SqlCommand command = new SqlCommand(query, connection);
+            //        SqlDataReader reader = command.ExecuteReader();
 
-                    // ملء ComboBox بالقيم من جدول items
-                    while (reader.Read())
-                    {
-                        ComboBoxStorgeCategories.Items.Add(reader["itemName"].ToString());
-                    }
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show($"Error loading item names: {ex.Message}");
-                }
-            }
+            //        // ملء ComboBox بالقيم من جدول items
+            //        while (reader.Read())
+            //        {
+            //            ComboBoxStorgeCategories.Items.Add(reader["itemName"].ToString());
+            //        }
+            //    }
+            //    catch (Exception ex)
+            //    {
+            //        MessageBox.Show($"Error loading item names: {ex.Message}");
+            //    }
+            //}
         }
 
 
         private void SaveChanges()
         {
-            try
-            {
-                dataAdapter.Update(dataTable);
-                MessageBox.Show("Changes saved successfully!");
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"Error saving changes: {ex.Message}");
-            }
+            //try
+            //{
+            //    dataAdapter.Update(dataTable);
+            //    MessageBox.Show("Changes saved successfully!");
+            //}
+            //catch (Exception ex)
+            //{
+            //    MessageBox.Show($"Error saving changes: {ex.Message}");
+            //}
         }
         private void guna2Button1_Click(object sender, EventArgs e)
         {
-            SaveChanges();
+            //SaveChanges();
         }
     }
 
