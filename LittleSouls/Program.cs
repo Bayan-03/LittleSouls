cdfe6b5052586e080f241638083a15415f6ea34a
bayan almerri <156858990+Bayan-03@users.noreply.github.com>
2024-11-26 01:07:06 +0300

    organaizing all forms in 3 folders
    petpath
    manegeerpath
    ceopath

--- LittleSouls/Program.cs
+++ LittleSouls/Program.cs
@@ -16,7 +16,7 @@ namespace LittleSouls
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Home());
+            Application.Run(new storageForm());
         }
     }
 }
